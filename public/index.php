cf775b691de9843b70364b4adb5300d85e4de65f
Administrator <fazo@qq.com>
2016-11-07 10:47:35 +0800

    Signed-off-by: Administrator <fazo@qq.com>

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 define("_SITE", 'index');
 define("_ROOT", realpath(__DIR__ . '/../') . '/');
-if (!require_once __DIR__ . "/../vendor/autoload.php") {
+if (!@include_once __DIR__ . "/../vendor/autoload.php") {
     exit('请先运行[composer install]');
 }
 
