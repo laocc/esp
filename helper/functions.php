518664ae92bf94876f563f065cf9838ac5eb9e79
Administrator <fazo@qq.com>
2016-11-11 16:09:00 +0800

    Signed-off-by: Administrator <fazo@qq.com>

--- helper/functions.php
+++ helper/functions.php
@@ -1,31 +1,4 @@
 <?php
-/*
- * 公用函数
- * 本文件所有函数基本可以用在任何其他程序中
- */
-/*
-
-pre(debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0]);
-
- */
-
-function pre(...$str)
-{
-    $prev = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
-    if (_CLI) {
-        if (isset($prev['file'])) echo "{$prev['file']}[{$prev['line']}]\n";
-        foreach ($str as $i => &$v) print_r($v);
-    } else {
-        if (isset($prev['file'])) {
-            $file = "<i style='color:blue;'>{$prev['file']}</i><i style='color:red;'>[{$prev['line']}]</i>\n";
-        } else {
-            $file = null;
-        }
-        echo "<pre style='background:#fff;display:block;'>", $file;
-        foreach ($str as $i => &$v) print_r($v);
-        echo "</pre>";
-    }
-}
 
 /**
  * 随机字符，唯一值用：uniqid(true)
@@ -108,64 +81,6 @@ function is_wap()
 function is_wechat()
 {
     return is_wap() === 2;
-}
-
-
-/**
- *
- * 产生一个错误信息，具体处理，由\plugins\Mistake处理
- * @param $str
- * @param int $level 错误级别，012，
- *
- * 0：系统停止执行，严重级别
- * 1：提示错误，继续运行
- * 2：警告级别，在生产环境中不提示，仅发给管理员
- *
- * error("{$filePath} 不是有效文件。");
- */
-function error($str, $level = 0, array $errFile = null)
-{
-    if (is_int($str)) goto state;
-    if (is_array($level)) list($level, $errFile) = [0, $level];
-
-    $err = $errFile ?: debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
-    \esp\core\Mistake::try_error($str, $level, $err);
-    return;
-
-    state:  //模拟成某个错误状态
-
-//    $err = $errFile ?: debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT | DEBUG_BACKTRACE_IGNORE_ARGS, 1)[0];
-//    pre($err);
-
-    $state = \esp\core\Config::states($str);
-    $server = isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : null;
-    $html = "<html>\n<head><title>{$str} {$state}</title></head>\n<body bgcolor=\"white\">\n<center><h1>{$str} {$state}</h1></center>\n<hr><center>{$server}</center>\n</body>\n</html>";
-    header_state($str, $state);
-    header('Content-type:text/html', true);
-    exit($html);
-}
-
-
-/**
- * 设置HTTP响应头
- * @param int $code
- * @param null $text
- * @throws Exception
- */
-function header_state($code = 200, $text = null)
-{
-    if (empty($code) OR !is_numeric($code)) {
-        error('状态码必须为数字');
-    }
-    if (empty($text)) {
-        $text = \esp\core\Config::states($code);
-    }
-    if (!stripos(PHP_SAPI, 'cgi')) {
-        header('Status: ' . $code . ' ' . $text, true);
-    } else {
-        $protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1';
-        header("{$protocol} {$code} {$text}", true, $code);
-    }
 }
 
 /**
@@ -325,70 +240,6 @@ function str_len($str)
     return $arr;
 }
 
-/**
- * 查询域名的根域名，兼容国别的二级域名
- * @param null $domain
- * @return null|string
- */
-function host($domain)
-{
-    $dm1 = 'cn|cm|my|ph|tw|uk|hk';
-    $dm2 = 'com|net|org|gov|idv|co|name';
-    if (strpos('/', $domain)) {
-        $domain = explode('/', $domain . '//')[2];
-    }
-    if (preg_match("/^(?:[\w\.\-]+\.)?([a-z]+)\.({$dm2})\.({$dm1})$/i", $domain, $match)) {
-        return "{$match[1]}.{$match[2]}.{$match[3]}";
-    } elseif (preg_match("/^(?:[\w\.\-]+\.)?([a-z]+)\.([a-z]+)$/i", $domain, $match)) {
-        return "{$match[1]}.{$match[2]}";
-    } else {
-        return null;
-    }
-}
-
-
-function server($key, $auto = null)
-{
-    $key = strtoupper($key);
-    return isset($_SERVER[$key]) ? $_SERVER[$key] : $auto;
-}
-
-/**
- * 加载文件，同时加载结果被缓存
- * @param $file
- * @return bool|mixed
- */
-function load($file)
-{
-    if (!$file) return false;
-    static $recode = [];
-    $file = root($file);
-    $md5 = md5($file);
-    if (isset($recode[$md5])) return $recode[$md5];
-    $recode[$md5] = include $file;
-    return $recode[$md5];
-}
-
-/**
- * 修正为_ROOT开头
- * @param string $path 若最后一个参数是true，则在返回结果后加/
- * @return string|array
- */
-function root(...$path)
-{
-    $len = count($path);
-    $folder = false;
-    if (is_bool($path[$len - 1])) {
-        $folder = $path[$len - 1];
-        unset($path[$len - 1]);
-    }
-    foreach ($path as $i => &$p) {
-        if (stripos($p, _ROOT) !== 0) $p = _ROOT . ltrim($p, '/');
-        if ($folder) $p = rtrim($p, '/') . '/';
-    }
-    return count($path) === 1 ? $path[0] : $path;
-}
-
 
 /**
  * 计算一个偶数的组成，比如：10=8+2，14=8+4+2，22=16+4+2。
@@ -463,44 +314,6 @@ function go($url, $js = false)
 }
 
 /**
- * @param $path
- * @param int $mode
- * @return bool
- * 文件权限：
- * 类型   所有者  所有者组    其它用户
- * r    read    4
- * w    write   2
- * x    exec    1
- * 通过PHP建立的文件夹权限一般为0740就可以了
- */
-function mk_dir($path, $mode = 0740)
-{
-    if (!$path) return false;
-    if (strrchr($path, '/')) $path = dirname($path);
-    if (file_exists($path)) return true;
-    if (!$mode) $mode = 0740;
-    try {
-        @mkdir($path, $mode, true);
-        return true;
-    } catch (\Exception $e) {
-        return true;
-    }
-}
-
-/**
- * 储存文件
- * @param $file
- * @param $content
- * @return int
- */
-function save_file($file, $content)
-{
-    if (is_array($content)) $content = json_encode($content, 256);
-    mk_dir($file);
-    return file_put_contents($file, $content, LOCK_EX);
-}
-
-/**
  * GB2312转UTF8
  * @param $str
  * @return string
@@ -544,7 +357,7 @@ function text($html, $star = null, $stop = null)
 
 
 //数字介于之间
-function between(int $n, int $a, int $b, bool $han = true)
+function between($n, $a, $b, $han = true)
 {
     return $han ? ($n >= $a and $n <= $b) : ($n > $a and $n < $b);
 }
@@ -627,24 +440,6 @@ function img_code($file, $split = false)
 }
 
 
-/**
- * XML解析
- * @param $str
- * @return bool|mixed
- */
-function xml_parser($str)
-{
-    libxml_disable_entity_loader(true);
-    $xml_parser = xml_parser_create();
-    if (!xml_parse($xml_parser, $str, true)) {
-        xml_parser_free($xml_parser);
-        return false;
-    } else {
-        $xml = @simplexml_load_string($str, 'SimpleXMLElement', LIBXML_NOCDATA);
-        return json_decode(json_encode($xml, 256), true);
-    }
-}
-
 
 /**
  * 时间友好型提示风格化（即XXX小时前、昨天等等）
@@ -652,7 +447,7 @@ function xml_parser($str)
  * @param int|null $time_now
  * @return string
  */
-function date_friendly(int $timestamp, int $time_now = null)
+function date_friendly($timestamp, $time_now = null)
 {
     $Q = $timestamp > time() ? '后' : '前';
     $V = $T = null;
