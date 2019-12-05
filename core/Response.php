c32c8745895df366f40d1d4fb6e6e3f9863dfacb
whdabai <1733886703@qq.com>
2019-12-06 00:13:01 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- core/Response.php
+++ core/Response.php
@@ -342,6 +342,10 @@ final class Response
             preg_match_all("/<link.*?href=['\"](.+?)['\"].*?\>/is", $html, $css, PREG_PATTERN_ORDER);
             $html = str_replace($css[0], '', $html);
             foreach ($css[1] as $i => $mch) {
+                if (substr($mch, 0, 4) === 'http' or substr($mch, 0, 2) === '//') {
+                    unset($css[1][$i]);
+                    continue;
+                }
                 if (($w = strpos($mch, '?')) > 0) {
                     $css[1][$i] = substr($mch, 0, $w);
                 }
@@ -353,6 +357,11 @@ final class Response
             preg_match_all("/<script.*?src=['\"](.+?)['\"]\><\/script>/i", $html, $jss, PREG_PATTERN_ORDER);
             $html = str_replace($jss[0], '', $html);
             foreach ($jss[1] as $i => $mch) {
+                if (substr($mch, 0, 4) === 'http' or substr($mch, 0, 2) === '//') {
+                    unset($jss[1][$i]);
+                    continue;
+                }
+
                 if (($w = strpos($mch, '?')) > 0) {
                     $jss[1][$i] = substr($mch, 0, $w);
                 }
