4cee4f2a45d50fe3b1c88e0c2d9c5b4879d2543a
whdabai <1733886703@qq.com>
2020-04-22 19:26:31 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -172,6 +172,7 @@ final class Dispatcher
                 if (getenv('HTTP_MOBILE')) {
                     $n = $this->_request->controller . '_' . $this->_request->action;
                     $testDebug['ua'] = getenv('HTTP_USER_AGENT');
+                    $testDebug['mob'] = getenv('HTTP_MOBILE');
                     $testDebug['file'] = $this->_debug->filename();
                     $testDebug['save'] = $save;
                     file_put_contents(_RUNTIME . '/debug/test/' . date('YmdHis_') . $n . '.txt', json_encode($testDebug, 64 | 128 | 256), LOCK_EX);
