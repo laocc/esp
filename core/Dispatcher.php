9689fbfebd7247704e2906f8f7414903136061fa
whdabai <1733886703@qq.com>
2020-05-15 16:48:41 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -42,6 +42,7 @@ final class Dispatcher
             $this->_debug = new Debug($this->_request, $this->_response, $debug);
             $GLOBALS['_Debug'] = &$this->_debug;
         }
+        
         if ($session = Config::get('session')) {
             $save = Session::_init($session, $option);
             //(($session['debug'] ?? 0) or !$save) and
