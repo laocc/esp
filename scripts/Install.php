2aa9497bec777e6c30c67240ccbffafe18b76457
LaoCC <fazo@qq.com>
2022-04-09 16:17:01 +0800

    Signed-off-by: LaoCC <fazo@qq.com>

--- scripts/Install.php
+++ scripts/Install.php
@@ -5,8 +5,54 @@ namespace esp\scripts;
 
 class Install
 {
-    public static function check()
+    private static function getRoot()
     {
-        echo "esp install/update \n";
+        if ($dirI = strpos(__DIR__, '/vendor/laocc/esp/core')) {
+            $rootPath = substr(__DIR__, 0, $dirI);
+        } else if ($dirI = strpos(__DIR__, '/laocc/esp/core')) {
+            $rootPath = (substr(__DIR__, 0, $dirI));
+        } else {
+            $rootPath = dirname($_SERVER['DOCUMENT_ROOT'], 2);
+        }
+        return $rootPath;
+    }
+
+    public static function install_pre()
+    {
+        echo date('Y-m-d H:i:s') . " Esp Install:\n ";
+    }
+
+    public static function update_pre()
+    {
+        echo date('Y-m-d H:i:s') . " Esp Update:\n ";
+    }
+
+    public static function install_post()
+    {
+        echo date('Y-m-d H:i:s') . " Install End;\n ";
+        echo "check runtime: ";
+        $root = self::getRoot();
+        if (!file_exists($root . '/runtime')) {
+            @mkdir($root . '/runtime', 0740, true);
+            @chown($root . '/runtime', 'www');
+            echo "mkdir\n";
+        } else {
+            echo "success\n";
+        }
+    }
+
+
+    public static function update_post()
+    {
+        echo date('Y-m-d H:i:s') . " Update End;\n ";
+
+        $root = self::getRoot();
+        if (!file_exists($root . '/runtime')) {
+            @mkdir($root . '/runtime/debug', 0740, true);
+            @chown($root . '/runtime/debug', 'www');
+            echo "mkdir\n";
+        } else {
+            echo "success\n";
+        }
     }
 }
